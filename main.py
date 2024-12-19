e221136cdb56278986721c52cfda8f5998a3275b
moslian-off <100130434+moslian-off@users.noreply.github.com>
2024-12-19 15:18:31 +0800

    4

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from scipy.sparse import csr_matrix
 from tqdm import tqdm
 import torch
 import matplotlib.pyplot as plt
-
+from sklearn.preprocessing import LabelEncoder
+from scipy.stats import mode
+from sklearn.metrics import accuracy_score
 
 def load_data():
   df = pd.read_excel("data/TicketData.xlsx")
   category = df["Sector"]
   change_ratio_list = df["ChangeRatioList"]
   change_ratio_array = []
+  
+  N =df.shape[0]
 
   for i in range(N):
     change_ratio_array.append(np.array(eval(change_ratio_list[i])).astype(int)) 
@@ -62,7 +66,7 @@ def to_pij(change_ratio_array):
     
     return result
 
-def compute_mutual_information(pi, pj, pij):
+def compute_mutual_information_from_possibility(pi, pj, pij):
     N = len(pij)
     result = np.zeros((N, N))
     epsilon = 1e-10 
@@ -85,6 +89,12 @@ def compute_mutual_information(pi, pj, pij):
     
     return result
 
+def compute_mutual_information(data):
+  pi = to_pi(data)
+  pij = to_pij(data)
+  mutual_info = compute_mutual_information_from_possibility(pi,pi,pij)
+  return normalize(mutual_info)
+  
 def normalize(mutual_info):
     D = np.diagonal(mutual_info)
     D_prime = 1 / np.sqrt(D)
@@ -107,13 +117,13 @@ def compute_s_C(P, similarity_matrix, C, N):
       s_C = np.sum(similarity_matrix * np.outer(P_column, P_column)) / norm_factor/(N**2)
     return s_C
 
-def GD_target_fc(P_C_i_temp, T):  
+def GD_target_fc(P_C_i_temp, T,similarity_matrix):  
     N, Nc = P_C_i_temp.shape
     P_C_i = P_C_i_temp/torch.sum(P_C_i_temp,dim = 1,keepdim = True)  
     P_C = compute_P_C(P_C_i,N)
     s_C = torch.zeros(Nc)
     for i in range(Nc):
-      s_C[i] = compute_s_C(P_C_i, mutual_info, i)
+      s_C[i] = compute_s_C(P_C_i, similarity_matrix, i)
     s_all_clusters = torch.dot(s_C, P_C)
     info = torch.sum(P_C_i * torch.log2(P_C_i / P_C)) / N
     return -s_all_clusters + T * info
@@ -187,189 +197,27 @@ def GD_method_to_cluster(T,similarity_matrix,N,Nc,epoch,lr=1e-3):
   labels = np.argmax(P_C_i,axis=1)
   return P_C_i.detach().numpy(),labels  
 
+def evaluate(pred,label,Nc):
+  y_encoded = LabelEncoder().fit_transform(label.to_numpy())
+  labels = np.zeros_like(pred)
+  for i in range(Nc):
+      mask = (pred == i)
+      labels[mask] = mode(y_encoded[mask],keepdims=True)[0][0]
+  accuracy = accuracy_score(y_encoded, labels)
+  return accuracy
+  
 def main():
   data,label = load_data()
-  N,D = data.shape
+  N = data.shape[0]
   Nc = 11
   T_GD = 1/60
   T_ia = 1/60
-  pi = to_pi(data)
-  pij = to_pij(data)
-  pearson_matrix = np.corrcoef(data)
-  mutual_info = compute_mutual_information(pi, pi, pij)
-  mutual_info = normalize(mutual_info)
-  GD_P_C_i = GD_method_to_cluster(T_GD,mutual_info,N,Nc,epoch=4000,lr =1e-3)
-  iterative_approach_P_C_i = iterative_approach_to_cluster(T_ia,mutual_info,N,Nc)
-  
-  
-def draw(P_C_i,i):
-    plt.figure(figsize=(8, 6))
-    x = np.arange(0, len(P_C_i[i]), 1)
-    plt.bar(x, P_C_i[i], width=0.8, color='skyblue', align='center')
-    plt.xticks(x)
-    plt.show()
-    print(np.max(P_C_i[i]))
+  mutual_info = compute_mutual_information(data)
+  GD_P_C_i,GD_pred = GD_method_to_cluster(T_GD,mutual_info,N,Nc,epoch=4000,lr =1e-3)
+  iterative_approach_P_C_i,iterative_pred = iterative_approach_to_cluster(T_ia,mutual_info,N,Nc)
+  acc = evaluate(GD_pred,label)
+  print(acc)
 
 
-N=496
-Nc = 11
-
-print(F)
-print(P_C)
-
-# %%
-entropy = []
-
-  
-
-plt.figure(figsize=(8, 6))
-X = np.arange(40,80,1)
-plt.plot(X, entropy) 
-plt.xticks(X)
-plt.show()
-
-# %%
-P_C_i = soft_clustering(mutual_info,1.0/60,Nc,epsilon)
-
-labels = np.argmax(P_C_i,axis=1)
-
-# %%
-from sklearn.preprocessing import LabelEncoder
-from scipy.stats import mode
-from sklearn.metrics import accuracy_score
-
-
-y_encoded = LabelEncoder().fit_transform(category.to_numpy())
-y_pred = np.argmax(P_C_i, axis=1)
-
-labels = np.zeros_like(y_pred)
-for i in range(Nc):
-    mask = (y_pred == i)
-    labels[mask] = mode(y_encoded[mask],keepdims=True)[0][0]
-
-accuracy = accuracy_score(y_encoded, labels)
-
-print(f"Accuracy:{accuracy:.4f}")
-
-# %% [markdown]
-# It can be found that the labels calculated by different method are the same.
-
-# %% [markdown]
-# | Nc  | k-means (Mutual Info) | Info-based (Mutual Info) | k-means (Pearson) | Info-based (Pearson) |
-# |-----|------------------------|--------------------------|-------------------|-----------------------|
-# | 20  | 0.157                 | 0.127                   | 0.384            | 0.354                |
-# | 10  | 0.131                 | 0.110                   | 0.320            | 0.307                |
-# | 5   | 0.113                 | 0.110                   | 0.264            | 0.276                |
-# 
-
-# %%
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Data
-Nc = [20, 10, 5]
-k_means_mutual_info = [0.157, 0.131, 0.113]
-info_based_mutual_info = [0.127, 0.110, 0.110]
-
-# Plot with four distinct columns for clarity
-plt.figure(figsize=(10, 6))
-
-# Adjust bar positions
-bar_width = 0.2
-x_indices = np.arange(len(Nc))
-
-# Mutual Information bars
-plt.bar(x_indices - 1.5 * bar_width, k_means_mutual_info, bar_width, label='k-means (Mutual Info)', color='skyblue')
-plt.bar(x_indices - 0.5 * bar_width, info_based_mutual_info, bar_width, label='Info-based (Mutual Info)', color='dodgerblue')
-
-# Labels and title
-plt.xlabel('Nc')
-plt.ylabel('<s>')
-plt.xticks(x_indices, Nc)
-plt.legend()
-plt.grid(axis='y', alpha=0.3)
-plt.tight_layout()
-plt.show()
-
-# %% [markdown]
-# ##### Testing
-
-# %%
-from sklearn.metrics import pairwise_distances
- 
-pearson_similarity_matrix = 1 - pairwise_distances(change_ratio_array, metric='correlation')
-
-print(pearson_similarity_matrix)
-plt.figure(figsize=(10, 8))
-plt.imshow(pearson_similarity_matrix)
-plt.colorbar()
-plt.show()
-
-# %%
-T = 1.0/80
-Nc = 11
-N = 150
-epsilon = 1e-4
-
-P_C_i = soft_clustering(pearson_similarity_matrix,T,Nc,epsilon)
-
-# %%
-from sklearn.preprocessing import LabelEncoder
-from scipy.stats import mode
-from sklearn.metrics import accuracy_score
-
-
-y_encoded = LabelEncoder().fit_transform(category.to_numpy())
-
-y_pred = np.argmax(P_C_i, axis=1)
-
-labels = np.zeros_like(y_pred)
-for i in range(Nc):
-    mask = (y_pred == i)
-    labels[mask] = mode(y_encoded[mask])[0][0]
-
-accuracy = accuracy_score(y_encoded, labels)
-
-print(f"Accuracy:{accuracy:.4f}")
-
-# %%
-from sklearn.cluster import KMeans
-
-kmeans = KMeans(n_clusters=11, random_state=42)
-kmeans.fit(change_ratio_array)
-
-y_pred = kmeans.labels_
-labels = np.zeros_like(y_pred)
-for i in range(Nc):
-    mask = (y_pred == i)
-    labels[mask] = mode(y_encoded[mask],keepdims=True)[0][0]
-
-accuracy = accuracy_score(y_encoded, labels)
-
-print(f"Kmeans Accuracy:{accuracy:.4f}")
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-
+if __name__ == '__main__':
+  main()
